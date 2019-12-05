6edfd4b2123b753e812c12ab8f480275ac3b4f89
Kristina-zh <kristina.zhuravleva1991@gmail.com>
2019-12-05 17:31:59 +0200

    Updates

---
+++ precache-manifest.3a54e0f9540e130c0013c7c429eee753.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a527e6c000f04eb7f9986cb88eb3f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63a4092eb50a9f091e75",
+    "url": "./static/css/main.4586248a.chunk.css"
+  },
+  {
+    "revision": "be4981e9db05a02b6706",
+    "url": "./static/css/movies-details-page.715b76e5.chunk.css"
+  },
+  {
+    "revision": "d933a509cb8bc342c7c1",
+    "url": "./static/js/0.2a3871fe.chunk.js"
+  },
+  {
+    "revision": "0898798f0520f53d23f2",
+    "url": "./static/js/6.4111a801.chunk.js"
+  },
+  {
+    "revision": "67262418685f1d75376c",
+    "url": "./static/js/home-page.092c0075.chunk.js"
+  },
+  {
+    "revision": "63a4092eb50a9f091e75",
+    "url": "./static/js/main.f74fbe01.chunk.js"
+  },
+  {
+    "revision": "be4981e9db05a02b6706",
+    "url": "./static/js/movies-details-page.722beccf.chunk.js"
+  },
+  {
+    "revision": "b854593b078525687744",
+    "url": "./static/js/movies-page.39e01b4f.chunk.js"
+  },
+  {
+    "revision": "c8d89cb31f40833e6111",
+    "url": "./static/js/runtime-main.ffc54653.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-movies/precache-manifest.426550e8da8cf18799b984e34e88f64d.js"
+  "./precache-manifest.3a54e0f9540e130c0013c7c429eee753.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-04-movies/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
