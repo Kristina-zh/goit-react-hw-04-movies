183ea1166e83dbb68002f8757141cbfbf64bc363
Kristina-zh <kristina.zhuravleva1991@gmail.com>
2019-12-05 16:57:09 +0200

    added build

---
+++ build/precache-manifest.bb78442fe27a2a292026a213efa4eb7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582db72f96e0d243e6ce69765eb55de1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3b3eb2748a08636a3fe",
+    "url": "/static/css/main.4586248a.chunk.css"
+  },
+  {
+    "revision": "d0b8013fd545135c66be",
+    "url": "/static/css/movies-details-page.715b76e5.chunk.css"
+  },
+  {
+    "revision": "21c8da8b5f9a1218da76",
+    "url": "/static/js/0.fa9fe238.chunk.js"
+  },
+  {
+    "revision": "0ae50a0493a5c8382554",
+    "url": "/static/js/6.b5ec212a.chunk.js"
+  },
+  {
+    "revision": "c966096894c7e5165e97",
+    "url": "/static/js/home-page.b2b61eb1.chunk.js"
+  },
+  {
+    "revision": "e3b3eb2748a08636a3fe",
+    "url": "/static/js/main.913276a5.chunk.js"
+  },
+  {
+    "revision": "d0b8013fd545135c66be",
+    "url": "/static/js/movies-details-page.caff3935.chunk.js"
+  },
+  {
+    "revision": "fe4c29da57f2c981c00c",
+    "url": "/static/js/movies-page.3bd3f90d.chunk.js"
+  },
+  {
+    "revision": "6d5e3c3c1ccd2c3f0a0f",
+    "url": "/static/js/runtime-main.11cdc43a.js"
+  }
+]);
